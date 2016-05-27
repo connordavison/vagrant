9f4b3804aeef8f4e707b2b3fef8ac5ddb9a26f76
C. Davison <connorpdavison@gmail.com>
2016-05-27 20:57:30 +0100

    Excluded Node & NPM from installation.

--- bootstrap/node.sh
+++ bootstrap/node.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -v
-# +-----------------------------+
-# | Installing NVM, Node, & NPM |
-# +-----------------------------+
+# +----------------+
+# | Installing NVM |
+# +----------------+
+export NVM_DIR="/home/vagrant/.nvm"
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash
-exec bash
-
-nvm install node
+echo "source ~/.nvm/nvm.sh" >> /home/vagrant/.bashrc
+chown -R vagrant:vagrant /home/vagrant/.nvm
