ea74d447ffc0f5bfd1bb9c0c4556011bc0042d19
C. Davison <connorpdavison@gmail.com>
2016-05-30 21:47:04 +0100

    Added xdebug toggler

--- bootstrap/bash_aliases.sh
+++ bootstrap/bash_aliases.sh
@@ -8,3 +8,11 @@ alias c='composer'
 alias artisan='php artisan'
 
 alias m='mysql -uroot'
+
+function xdebug {
+    if [ "$1" == "off" ]; then
+        sudo phpdismod xdebug
+    else
+        sudo phpenmod xdebug
+    fi
+}
